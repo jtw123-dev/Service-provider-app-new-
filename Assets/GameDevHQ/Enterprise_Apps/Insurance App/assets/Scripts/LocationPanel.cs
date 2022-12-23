c4470ee6c4873a9d27aecf0bed9c0977b82811a3
Joshua Watts <joshwatts592@gmail.com>
2022-12-23 14:57:17 -0330

    Finished camera and android functionality

--- Assets/GameDevHQ/Enterprise_Apps/Insurance App/assets/Scripts/LocationPanel.cs
+++ Assets/GameDevHQ/Enterprise_Apps/Insurance App/assets/Scripts/LocationPanel.cs
@@ -6,10 +6,20 @@ using UnityEngine.UI;
 public class LocationPanel : MonoBehaviour {
     [SerializeField] private RawImage _map;
     [SerializeField] private InputField _mapNotes;
+    [SerializeField] private Text _caseNumberTitle;
+
+    public void OnEnable()
+    {
+        _caseNumberTitle.text = "CASE NUMBER" + UIManager.Instance.activeCase.caseID;
+    }
+
 
     public void ProcessInfo()
     {
-
+        if (string.IsNullOrEmpty(_mapNotes.text)==false)
+        {
+            UIManager.Instance.activeCase.locationNotes = _mapNotes.text;
+        }
     }
 	
 }
